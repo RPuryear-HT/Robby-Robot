9947332303f24999c251516b3d92bd99beb7584c
Robert Puryear <126106985+RPuryear-HT@users.noreply.github.com>
2025-01-28 11:14:18 -0500

    Update Delegate-O365-OnPrem-Rights-v3.ps1

--- Exchange/Delegate-O365-OnPrem-Rights-v3.ps1
+++ Exchange/Delegate-O365-OnPrem-Rights-v3.ps1
@@ -1,10 +1,19 @@
 ﻿# Add on-prem "Send As" rights to an O365 mailbox. Method 3 of 3.
 
+# Set script to terminate upon an error
+$ErrorActionPreference = 'Stop'
+
 # Prompt the user to enter the mailbox logon name to delegate rights
 $delegatedbox = Read-Host "Enter the mailbox logon name to delegate rights."
 
 # Prompt the user to enter the target user's O365 email address
 $delegateduser = Read-Host "Enter the target user's O365 email address."
+
+# Create a new PowerShell session to connect to the on-premises Exchange server
+$session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://hcvpwex0.hteeter.ht/PowerShell -Authentication Kerberos
+
+# Import the PowerShell session created for Exchange, allowing clobbering of existing commands and suppress warning banner
+Import-PSSession $session -AllowClobber 3>$null
 
 # Grant "Send on Behalf" permission to the specified mailbox for the target user
 Set-Mailbox "$delegatedbox" -GrantSendOnBehalfTo @{add="$delegateduser"}
