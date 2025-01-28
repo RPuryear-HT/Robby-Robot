5df4b03629ac687e6dd3898a28d4d0a7635d359f
Robert Puryear <126106985+RPuryear-HT@users.noreply.github.com>
2025-01-28 11:02:06 -0500

    Update Delegate-O365-OnPrem-Rights-v2.ps1

--- Exchange/Delegate-O365-OnPrem-Rights-v2.ps1
+++ Exchange/Delegate-O365-OnPrem-Rights-v2.ps1
@@ -6,6 +6,12 @@ $delegatedbox = Read-Host "Enter the group name to delegate rights"
 # Prompt the user to enter the user's reply address
 $delegateduser = Read-Host "Enter the user's reply address"
 
+# Create a new PowerShell session to connect to the on-premises Exchange server
+$session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://<YourExchangeServerAddress>/PowerShell -Authentication Kerberos
+
+# Import the PowerShell session created for Exchange, allowing clobbering of existing commands and suppress warning banner
+Import-PSSession $session -AllowClobber 3>$null
+
 # Add "Send As" permission to the specified mailbox for the target user
 Get-Mailbox "$delegatedbox@<YourDomain>.com" | Add-ADPermission -User "$delegateduser@<YourDomain>.com" -ExtendedRights "Send As"
 
