abe55ede242695f6cad50c55aa3d36538582e8d5
Robert Puryear <126106985+RPuryear-HT@users.noreply.github.com>
2025-01-28 09:42:34 -0500

    Update Set-Password-Change.ps1
    Set "change password at next logon" value to true. Requires a list of users in (C:\scripts\changepassword.txt).

--- Active Directory/Set-Password-Change.ps1
+++ Active Directory/Set-Password-Change.ps1
@@ -1,6 +1,12 @@
 # Set "change password at next logon" value to true. Requires a list of users in (C:\scripts\changepassword.txt).
+
+# Read the list of users from the specified text file
 $users = Get-Content C:\scripts\changepassword.txt
+
+# Loop through each user in the list
 foreach ($user in $users) {
+    # Set the user to change their password at next logon
     Set-ADUser -identity $user -ChangePasswordAtLogon $true
     }
-# Created by Robert Puryear # Updated 10/19/2023
+
+# Created by Robert Puryear 10/19/2023 # Updated 1/28/2025
