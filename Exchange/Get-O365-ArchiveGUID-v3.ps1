2810a442cabf01064ac5b2ee575f0bbb51818e3b
Robert Puryear <126106985+RPuryear-HT@users.noreply.github.com>
2025-01-31 08:06:09 -0500

    Update Get-O365-ArchiveGUID-v3.ps1

--- Exchange/Get-O365-ArchiveGUID-v3.ps1
+++ Exchange/Get-O365-ArchiveGUID-v3.ps1
@@ -60,6 +60,9 @@ if (-not (Test-Path -Path $path)) {
 # Import the Exchange Online Management module
 Import-Module ExchangeOnlineManagement
 
+# Prompt user to check for the Exchange Online logon window
+Write-Host "Waiting for user to authenticate.. Please check the Exchange Online logon window to continue.."
+
 # Connect to Exchange Online without showing the banner
 Connect-ExchangeOnline -ShowBanner:$false
 
