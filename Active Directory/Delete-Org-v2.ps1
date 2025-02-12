c920bfd467cdcb71d5b84675f96794bc2ab5c692
Robert Puryear <126106985+RPuryear-HT@users.noreply.github.com>
2025-02-12 13:16:44 -0500

    Update Delete-Org-v2.ps1

--- Active Directory/Delete-Org-v2.ps1
+++ Active Directory/Delete-Org-v2.ps1
@@ -21,26 +21,11 @@
                                                                                           
 Description: Clear the manager for a list of AD users.
 ==============================================================================================================================
-Note: Must have a properly formatted csv file containing target usernames. Below are 2 examples of formatting needing for the 
-csv file. Either works, but separate columns is easier to manipulate. 
-
-Make sure your CSV file has the Username header. For example:
-
-(1) Example of csv format:
-
-|Manager|Username| <<<<<<<<<< Header
-|boss1|user1 | 
-|boss2|user2 | 
-
-(2) Example of csv format:
-
-Username,Manager       <<<<<<<<<< Header
-user1,boss1    
-user2,boss2 
+Note: Must have a text file containing target usernames located at 'C:\scripts\deleteorg.txt'.
 ==============================================================================================================================
 Author: Robert Puryear
 ==============================================================================================================================
-Last Revision: 1/27/2025
+Last Revision: 2/12/2025
 ==============================================================================================================================
 
    ____ _                            _             
@@ -58,6 +43,9 @@ Added ASCII art.
 Added code to remove the variables used.
 Added notes to the code.
 Added code execution timer. Formatted run time output message.
+2/12/2025
+Edited code to import from a text fle, instead of CSV.
+Edited code notes.
 ==============================================================================================================================
 
 #>
@@ -65,22 +53,17 @@ Added code execution timer. Formatted run time output message.
 # Import the Active Directory module
 Import-Module ActiveDirectory
 
-# Define the path to the CSV file containing the usernames
-$csv = "C:\scripts\deleteorg.csv"
+# Import the file containing the usernames
+$userlist = Get-Content "C:\scripts\deleteorg.txt"
 
-# Import the CSV file
-$userlist = Import-Csv -Path $csv
-
-# Loop through each user in the CSV file
+# Loop through each user in the text file
 foreach ($user in $userlist) {
-    # Get the username from the CSV
-    $username = $user.Username
 
     # Clear the manager attribute in Active Directory
-    Set-ADUser -Identity $username -Clear Manager
+    Set-ADUser -Identity $user -Clear Manager
 
     # Output the result for each user
-    Write-Output "Cleared manager for user $username" | Out-Host
+    Write-Output "Cleared manager for user $user" | Out-Host
 }
 
 # Output a message indicating the manager clearing process is complete
